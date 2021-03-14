f1ac7086fb305331735fc124397ecb04ff429bb3
markus <markus_marks@web.de>
2021-03-14 22:26:49 +0100

    add DiagSensorboard

---
+++ src/variometer/DiagSensorboard.cpp
@@ -0,0 +1,10 @@
+/*
+ * DiagSensorboard.cpp
+ *
+ *  Created on: Mar 14, 2021
+ *      Author: markus
+ */
+
+
+
+
---
+++ src/variometer/DiagSensorboard.h
@@ -0,0 +1,15 @@
+/*
+ * DiagSensorboard.h
+ *
+ *  Created on: Mar 14, 2021
+ *      Author: markus
+ */
+
+#ifndef SRC_VARIOMETER_DIAGSENSORBOARD_H_
+#define SRC_VARIOMETER_DIAGSENSORBOARD_H_
+
+
+
+
+
+#endif /* SRC_VARIOMETER_DIAGSENSORBOARD_H_ */
